d822a94a16cc28b099aa85e20df80711bb1607ce
Erick Delgado <127866310+BanyelaneYT@users.noreply.github.com>
2026-09-23 00:17:24 -0500

    Update ApiKeys.kt

--- app/src/main/java/com/example/jarvisapp/ai/ApiKeys.kt
+++ app/src/main/java/com/example/jarvisapp/ai/ApiKeys.kt
@@ -2,5 +2,8 @@ package com.example.jarvisapp.ai
 
 object ApiKeys {
     // Reemplaza con tus keys reales
+    const val GROQ_API_KEY = "gsk_xxxxxxxxxxxxxxxxxxxxx"
+    const val GEMINI_API_KEY = "AIzaSyxxxxxxxxxxxxxxxxxxxx"
+    const val OPENAI_API_KEY = "sk-xxxxxxxxxxxxxxxxxxxx"   // opcional
     const val XAI_API_KEY = "xai-xxxxxxxxxxxxxxxxxxxx"     // opcional (Grok)
 }
